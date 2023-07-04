1ef076b4854c2e275626e4bb61d597fcc14e8852
silvia40 <silvia40@naver.com>
2023-07-04 17:15:43 +0900

    삼각형의 모양을 변경함
    고객요구사항

--- Triangle.java
+++ Triangle.java
@@ -2,12 +2,12 @@
 class Triangle {
 
 	public static void main(String[] args) {
-		System.out.println("     @");
-		System.out.println("    @@");
-		System.out.println("   @@@");
-		System.out.println("  @@@@");
-		System.out.println(" @@@@@");
 		System.out.println("@@@@@@");
+		System.out.println(" @@@@@");
+		System.out.println("  @@@@");
+		System.out.println("   @@@");
+		System.out.println("    @@");
+		System.out.println("     @");
 
 	}
 
