41952a5adebb2ecee2016beb3b18a068aa87f8ac
silvia40 <silvia40@naver.com>
2023-07-06 14:34:06 +0900

    3

--- src/Hello.java
+++ src/Hello.java
@@ -13,18 +13,18 @@ public class Hello {
 
 		System.out.print("\033[5;10H");
 		System.out.println("Hello javaScript");
-
-		
-		System.out.println("\033[34m");
-		
-		System.out.print("\033[20;10H");
-		System.out.println("Hello HTML");
-		
+	
 		System.out.println("\033[31m");
 		System.out.println("\033[44m");
 
 		System.out.print("\033[15;25H");
 		System.out.println("Hello SQL");
+		
+		System.out.println("\033[34m");
+		System.out.println("\033[41m");
+
+		System.out.print("\033[20;10H");
+		System.out.println("Hello HTML");
 		System.out.println("\033[0m");
 	}
 
