ea1420432ac0d3321b0aa58f1b321a16f68f6b92
silvia40 <silvia40@example.com>
2023-07-06 12:10:07 +0900

    color

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 public static void main(String[] args){
 	System.out.println("\033[2J");
+	System.out.println("\033{31m");
+System.out.println("\033{44m");
 	System.out.println("Hello java");
 
 
