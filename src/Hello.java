fbd38b82b0412acdf69c638caef87543d1afbc0c
silvia40 <silvia40@naver.com>
2023-07-06 12:01:13 +0900

    vt100 clear

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,15 @@
+public class Hello {
+public static void main(String[] args){
+	System.out.println("\033[2J");
+	System.out.println("Hello java");
 
-class Hello {
-	public static void main(string[] args) {
+
+}
+
+
+
+
+
+
 	
-
-
-
-
-
-
-	}
 }
